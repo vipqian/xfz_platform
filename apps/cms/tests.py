d8910af25b6589c88e54f609a730234883eb3735
vipqian <qianvipfei@163.com>
2019-08-22 17:08:34 +0800

    修改了uwsgi.ini

--- apps/cms/tests.py
+++ apps/cms/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+import unittest
+
+class TestObject(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print('start' + '+'*100)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        print('end' + '+'*100)
+
+
+    def setUp(self):
+        print('开始')
+
+    def tearDown(self) -> None:
+        print('结束')
+
+
+    def test_a(self):
+        self.assertEqual(1, 1)
+
+    def test_b(self):
+        self.assertEqual(2, 1)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
